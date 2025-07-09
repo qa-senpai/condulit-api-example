f5e2ed71dd5f5b8503e84b58251fc71a43195f8a
qa_coach <psp@sitecore.net>
2025-07-09 19:12:43 +0300

    feat: Add logging for OTHER_PASSWORD in environment variable tests

--- tests/api/envVariable.api.ts
+++ tests/api/envVariable.api.ts
@@ -3,4 +3,7 @@ import { test } from "../fixture/api-fixture";
 test("check env variable", async ({ articleController }) => {
   console.log(process.env.WORKERS_COUNT);
   console.log(process.env.BASE_URL);
+
+  console.log(process.env.CONDULIT_DEFAULT_PASSWORD);
+  console.log(process.env.OTHER_PASSWORD);
 });
