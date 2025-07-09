64eb6ce673f5226007b1127917cadcca4d0b75d9
qa_coach <psp@sitecore.net>
2025-07-09 18:48:15 +0300

    feat: Convert WORKERS_COUNT to a number for proper configuration in Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.WORKERS_COUNT ? process.env.WORKERS_COUNT : 1,
+  workers: Number(process.env.WORKERS_COUNT ? process.env.WORKERS_COUNT : 1),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
 
